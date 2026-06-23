19db5ca2be3840f6d66a0c761f74a85764bd8e14
Md Saiful Haque <saifulhaque.pciu@gmail.com>
2026-06-23 22:41:39 +0600

    initialize project and npm setup

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import app from "./app"
+import config from "./config";
+// import { prisma } from "./lib/prisma";
+import "dotenv/config";
+
+const PORT = config.port;
+
+async function main () {
+    try {
+        // await prisma.$connect();
+        console.log("connected to db");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        })
+    } catch (error) {
+        console.log(error);
+        // await prisma.$disconnect()
+        process.exit(1);
+    }
+}
+
+main();
