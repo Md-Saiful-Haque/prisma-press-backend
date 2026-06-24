5a0fe9a103d07c9c2b49672b3b4aefcd7920ce6f
Md Saiful Haque <saifulhaque.pciu@gmail.com>
2026-06-24 13:07:02 +0600

    generated prisma client and users registered successfully

--- src/app.ts
+++ src/app.ts
@@ -4,23 +4,24 @@ import express, { Application, Request, Response } from "express";
 import config from "./config";
 import { prisma } from "./lib/prisma";
 import bcrypt from "bcryptjs";
+import httpStatus from "http-status";
 
-const app : Application = express();
+const app: Application = express();
 
 app.use(cors({
-    origin : config.app_url,
-    credentials : true,
+    origin: config.app_url,
+    credentials: true,
 }))
 
 app.use(express.json());
-app.use(express.urlencoded({ extended : true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
-app.get("/", async(req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
     res.send("Hello from Prisma Press")
 })
 
-app.get("/api/users/register", async(req: Request, res: Response) => {
+app.post("/api/users/register", async (req: Request, res: Response) => {
     const { name, email, password, profilePhoto } = req.body;
 
     const isUserExist = await prisma.user.findUnique({
@@ -32,6 +33,43 @@ app.get("/api/users/register", async(req: Request, res: Response) => {
     }
 
     const hashedPassword = await bcrypt.hash(password, Number(config.bcrypt_salt_rounds))
+
+    const createdUser = await prisma.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        }
+    })
+
+    await prisma.profile.create({
+        data: {
+            userId: createdUser.id,
+            profilePhoto
+        }
+    })
+
+    const user = await prisma.user.findUnique({
+        where: {
+            id: createdUser.id,
+            email: createdUser.email || email
+        },
+        omit: {
+            password: true
+        },
+        include: {
+            profile: true
+        }
+    })
+
+    res.status(httpStatus.CREATED).json({
+        success: true,
+        statusCode: httpStatus.CREATED,
+        message: "User registered successfully",
+        data: {
+            user
+        }
+    });
 
 })
 
