c362a7fec1965a66715b7c87f6cd4cfa2118a281
Md Saiful Haque <saifulhaque.pciu@gmail.com>
2026-06-24 12:28:28 +0600

    created user and profile prisma model

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import config from "./config";
-
+import { prisma } from "./lib/prisma";
+import bcrypt from "bcryptjs";
 
 const app : Application = express();
 
@@ -15,8 +16,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
 app.use(cookieParser())
 
-app.get("/", (req, res) => {
+app.get("/", async(req: Request, res: Response) => {
     res.send("Hello from Prisma Press")
+})
+
+app.get("/api/users/register", async(req: Request, res: Response) => {
+    const { name, email, password, profilePhoto } = req.body;
+
+    const isUserExist = await prisma.user.findUnique({
+        where: { email }
+    })
+
+    if (isUserExist) {
+        throw new Error("User with this email already exists");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, Number(config.bcrypt_salt_rounds))
+
 })
 
 export default app;
