530f49cb0f0e2b90d2e9db25d8c8d31fceabedeb
Md Saiful Haque <saifulhaque.pciu@gmail.com>
2026-06-27 23:56:18 +0600

    implementing auth middleware

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -56,12 +56,24 @@ const createUser = catchAsync(async (req: Request, res: Response, next: NextFunc
 })
 
 const getMyProfile = catchAsync (async (req: Request, res: Response, next: NextFunction) => {
-    const {accessToken} = req.cookies;
-    console.log(accessToken);
-    
-    const verifiedToken = jwtUtils.verifyToken(accessToken, config.jwt_access_secret)
 
-    const profile = await userService.getMyProfileFromDB(verifiedToken.id)
+    // const {accessToken} = req.cookies;
+    // console.log(req.user, "user request");
+
+    // const verifiedToken = jwtUtils.verifyToken(accessToken, config.jwt_access_secret)
+
+    // if(typeof verifiedToken === "string"){
+    //     throw new Error(verifiedToken);
+    // }
+
+    const profile = await userService.getMyProfileFromDB(req.user?.id as string);
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "User profile fetched successfully",
+        data: { profile }
+    })
 })
 
 export const userController = {
